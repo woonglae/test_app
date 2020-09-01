0ac3de634dd02d5cd8c0db4d0556984a917702ed
Woongrae Cho <woonglae@gmail.com>
2020-09-01 15:56:53 -0400

    add temp data

---
+++ tempData.js
@@ -0,0 +1,56 @@
+export default tempData = [
+    {
+        name : "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title:"Book Flight",
+                completed: false
+            },
+            {
+                title:"Passport Check",
+                completed: true
+            },
+            {
+                title:"Reserve Hotel",
+                completed: true
+            },
+            {
+                title:"Pack Luggage",
+                completed: true
+            }
+        ]
+    },
+    {
+        name : "Grocery",
+        color : "#8022D9",
+        todos : [
+            {
+                title:"Curry",
+                completed: false
+            },
+            {
+                title:"Milk",
+                completed: false
+            },
+            {
+                title:"Orange",
+                completed: false
+            }        
+        ]
+    },
+    {
+        name : "Course",
+        color : "#6072D9",
+        todos : [
+            {
+                title:"CS4235",
+                completed: false
+            },
+            {
+                title:"CS4261",
+                completed: false
+            }
+        ]
+    }
+]
